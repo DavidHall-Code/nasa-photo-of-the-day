ac12967ab8309bbfc65b6c61714ebb112becda39
DavidHall-Code <david_m_hall@outlook.com>
2020-10-07 17:47:41 -0400

    Set useState and useEffect on App.js. Also setup axios to retrive from Nasa API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios"
 import "./App.css";
 
+// API address = https://api.nasa.gov/planetary/apod
+
 function App() {
+  const [data, setData] = useState([]);
+  const [count, setCount] = useState(0);
+
+  useEffect(()=> {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=PGZGMrCnZwWQRecXqsROalxcmvyhygGql0iLL1Of')
+    .then((res) => {
+      console.log('this is the Nasa API data: ', res);
+      setData(res.data);
+    })
+    .catch((err) => {
+      console.log('An ERROR has populated: ', err );
+    })
+
+  }, []);
+
+ 
   return (
     <div className="App">
       <p>
