c163b120b260598ad93bf1e984ba92ab4bb12f73
David Hall <david@WebDevelopment.home>
2020-11-04 17:25:03 -0500

    Setup API in app.js started putting together components folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import Space from './spaceContainer'
+
 import "./App.css";
+import axios from "axios";
 
 function App() {
+
+  const [data, setData] = useState([])
+  const [count, setCount] = useState(0)
+
+  useEffect (() => {
+    axios
+      .get ('https://api.nasa.gov/planetary/apod?api_key=SboTt1nag1tEyLWthGglQu79e3tMpAx4avV9OGhR')
+      .then ((res) => {
+        console.log('This is the data from the API: ', res)
+        setData(res.data)
+      })
+      .catch ((err) => {
+        console.log('Ther was an ERROR: ', err)
+      })
+
+
+  })
+
   return (
     <div className="App">
       <p>
